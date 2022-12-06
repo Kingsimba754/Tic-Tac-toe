5ee8a94cbb9a0d753f1574b6b235e8370b926b06
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2022-12-06 11:36:27 -0500

    added font link to HTML and buttons are now working in JS

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,37 @@
-///////////
-//Variables///
-///////////
-let playertext = document.getElementById('playerText')
+let playerText = document.getElementById('playerText')
 let restartBtn = document.getElementById('restartBtn')
 let boxes = Array.from(document.getElementsByClassName('box'))
 
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
 
 const O_TEXT = "O"
 const X_TEXT = "X"
 let currentPlayer = X_TEXT
 let spaces = Array(9).fill(null)
 
-let winnerIndicator =  get getComputedStyle(document.body).getPrpertyValue('--winning-blocks')
-
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
 
-function boxClicked(e){
+function boxClicked(e) {
     const id = e.target.id
 
     if(!spaces[id]){
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
-        if(playerHasWon(!==false)){
-            playertext = `${currentPlayer} has won!!`
-            let winning_Blocks = playerHasWon()
+
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} has won!`
+            let winning_blocks = playerHasWon()
+
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            return
         }
 
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT: X_TEXT
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
     }
 }
+
 const winningCombos = [
     [0,1,2],
     [3,4,5],
@@ -41,11 +42,12 @@ const winningCombos = [
     [0,4,8],
     [2,4,6]
 ]
-function playerHasWon(){
+
+function playerHasWon() {
     for (const condition of winningCombos) {
-        let [a, b, c]= condition
-        
-        if(spaces[a] && (spaces[a] == spaces[b] && spaces [a] == spaces [c])){
+        let [a, b, c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
             return [a,b,c]
         }
     }
@@ -54,12 +56,16 @@ function playerHasWon(){
 
 restartBtn.addEventListener('click', restart)
 
-function restart(){
+function restart() {
     spaces.fill(null)
-    boxes.forEach(box => {
+
+    boxes.forEach( box => {
         box.innerText = ''
+        box.style.backgroundColor=''
     })
-    playertext = `TIC TAC TOE`
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
     currentPlayer = X_TEXT
 }
 
